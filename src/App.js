4ead1ef499e97f44eb5ebc33e559c3df723c71e9
Hamza Dev <devhamzak@gmail.com>
2023-03-30 14:38:28 +0500

    Push

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Account from './pages/Account'
+import Home from './pages/Home'
 
 const App = () => {
+  const user = false
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={user ? <Home /> : <Account />}>
+          <Route index element={<h1>test</h1>} />
+        </Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/pages/Account.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Account = () => {
+  return (
+    <div className='w-[100%] h-[100vh] flex justify-center items-center'>
+        <div className='w-[60%] h-[60%] border-[1px] border-black'>
+        </div>
+    </div>
+  )
+}
+
+export default Account
